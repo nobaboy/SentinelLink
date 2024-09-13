235058385dde72829f52eabdcba7d6f7eb28702f
GitHub Action <action@github.com>
2024-09-13 02:16:09 +0000

    Initialize template repository proper
    Set package name to com.github.nobaboy.sentinellink
    Set project name to SentinelLink
    Set modid to sentinellink

--- src/main/kotlin/com/github/nobaboy/sentinellink/ExampleMod.kt
+++ src/main/kotlin/com/github/nobaboy/sentinellink/ExampleMod.kt
@@ -1,4 +1,4 @@
-package com.example
+package com.github.nobaboy.sentinellink
 
 import net.minecraft.client.Minecraft
 import net.minecraft.init.Blocks
@@ -6,7 +6,7 @@ import net.minecraftforge.fml.common.Mod
 import net.minecraftforge.fml.common.event.FMLInitializationEvent
 import net.minecraft.client.renderer.GlStateManager
 
-@Mod(modid = "examplemod", useMetadata = true)
+@Mod(modid = "sentinellink", useMetadata = true)
 class ExampleMod {
     @Mod.EventHandler
     fun init(event: FMLInitializationEvent) {
--- src/main/kotlin/com/github/nobaboy/sentinellink/mixin/MixinGuiMainMenu.java
+++ src/main/kotlin/com/github/nobaboy/sentinellink/mixin/MixinGuiMainMenu.java
@@ -1,4 +1,4 @@
-package com.example.mixin;
+package com.github.nobaboy.sentinellink.mixin;
 
 import net.minecraft.client.gui.GuiMainMenu;
 import org.spongepowered.asm.mixin.Mixin;
